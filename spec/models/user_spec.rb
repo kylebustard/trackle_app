d052e08436ec86a07a8d574015d70b28f00ecfc9
Kyle Bustard <bustardkyle@gmail.com>
2016-12-20 22:52:36 -0500

    create index action for posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "creation" do
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow", company: 'Facebook')
+      @user = User.create(email: "test@test.com",
+        password: "asdfasdf", password_confirmation: "asdfasdf",
+        first_name: "Jon", last_name: "Snow", company: 'Facebook')
     end
 
     it "can be created" do
