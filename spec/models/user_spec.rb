44c53ca4dd2f24d54529f8c830c4dcbeb5760904
Kyle Bustard <bustardkyle@gmail.com>
2017-01-08 23:08:31 -0500

    integrate phone validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe User, type: :model do
     it "can be created" do
       expect(@user).to be_valid
     end
+  end
 
+  describe "validation" do
     it "cannot be created without first_name" do
       @user.first_name = nil
       expect(@user).to_not be_valid
@@ -23,6 +25,16 @@ RSpec.describe User, type: :model do
     it "cannot be created without phone" do
       @user.phone = nil
       expect(@user).to_not be_valid
+    end
+
+    it 'requires the phone attr to only contain integers' do
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end
+
+    it 'requires the phone attr to only have 10 chars' do
+      @user.phone = '12345678901'
+      expect(@user).to_not be_valid 
     end
   end
 
