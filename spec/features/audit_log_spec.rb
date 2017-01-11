fea6066db85bb6ea671b1408339c842a27e402b2
Kyle Bustard <bustardkyle@gmail.com>
2017-01-10 23:36:15 -0500

    block non admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -7,7 +7,7 @@ describe 'AuditLog feature' do
 			login_as(admin_user, :scope => :user)
 			FactoryGirl.create(:audit_log)
 		end 
-		
+
 		it 'has an index page that can be reached' do
 			visit audit_logs_path
 			expect(page.status_code).to eq(200)
@@ -15,11 +15,17 @@ describe 'AuditLog feature' do
 
 		it 'renders audit log content' do
 			visit audit_logs_path
-			expect(page).to have_content(/SNOW/)
+			expect(page).to have_content(/SNOW, JON/)
 		end
 
-		xit 'cannot be accessed by non admin users' do
+		it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
 
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
 		end
 	end
 end
