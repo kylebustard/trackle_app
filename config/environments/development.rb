461b43fbe1e3999929a65e311b35db5133a869e9
Kyle Bustard <bustardkyle@gmail.com>
2017-01-17 19:41:43 -0500

    integrate basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,25 +1,14 @@
-Rails.application.configure do  config.cache_classes = false
+Rails.application.configure do
+  config.cache_classes = false
   config.eager_load = false
-  config.consider_all_requests_local = true
-  if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
-  config.assets.quiet = true
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
